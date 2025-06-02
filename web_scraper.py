1322aa9385e269ec28a2f2f6962591d743719960
Nawat Vatanawood <nawatv@gmail.com>
2025-06-02 22:51:39 +0700

    Added 'search for stock' & func: clrPopup.

--- web_scraper.py
+++ web_scraper.py
@@ -1,12 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
+from selenium.webdriver.common.action_chains import ActionChains
+# Use copy & paste
+import pyperclip
 # Use pathlib to work with paths (optional)
 from pathlib import Path
 
-#As of 1/June/2025 
-# ============= Full XPATH + Click (SET(eng)) =============================
+#As of 2/June/2025 
+# ============= Scrape data | Click ele | Search for stock [SET(eng)] =============================
+#               Full XPATH 
 
 # Set up the path to chromedriver
                     ### PATH !!!
@@ -18,38 +25,46 @@ driver = webdriver.Chrome(service=service)
 # Open a website: SET (eng)
 driver.get("https://www.set.or.th/en/home")
 # Wait
-time.sleep(4) #<-3
+time.sleep(3)
+
+# Function
+def clrPopup():
+    try:
+        e_x = None
+        e_x = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[3]/div/div[1]/button")
+        if e_x is not None:
+            e_x.click()
+    except:
+        print("No 'X' button found.")
+
+# Long-term vars
+li1_data = []   # Storing [idx, last]
+li2_data = []   # Storing 'Price Info'
+li3_data = []   # Storing 'Highlights'
 
 
 # Pre-find an element
-## Exit a pop-up if it exists [Click]
-try:
-    e_x = None
-    e_x = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[3]/div/div[1]/button")
-    if e_x is not None:
-        e_x.click()
-except:
-    print("No 'X' button found.")
-    
+## Clear the pop-up if it exists [Click]
+clrPopup()
+
 ## Accept all the cookies [Click | Full XPATH + By.CLASS & TAG_NAME]
 try:
     #--- Full XPATH ---
-    e_acc = None    
-    e_acc = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[7]/div/div/div[2]/button")
-    if e_acc is not None:
-        e_acc.click()
+    e_x = None
+    e_x = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[7]/div/div/div[2]/button")
+    if e_x is not None:
+        e_x.click()
 
     #--- By.CLASS & TAG_NAME ---
     # e_acc_block = driver.find_element(By.CLASS_NAME, "pdpa")
     # e_acc = e_acc_block.find_element(By.TAG_NAME, "button")
     # if e_acc is not None:
     #    e_acc.click()
-  
+        
 except:
     print("No 'Accept cookies' button found.")
-
+    
 # Find an element
-li_data = []
 for i in range(10):                      
     e_col_path = "/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/div[2]/div[1]/div/table[2]/tbody/"
     e_idx_col = None
@@ -66,10 +81,43 @@ for i in range(10):
     except:
         print("No e_last_col_" + str(i+1) + " found.")
 
-    li_data.append([e_idx_col.text, e_last_col.text])
+    li1_data.append([e_idx_col.text, e_last_col.text])
 
-# Print li_data
-print(li_data)
+# Print li1_data
+print(li1_data)
+
+# Find & search in a search box [pyperclip & js] [Must use]
+try:
+    #--- Recognize an input ---
+    e_srh_path = "/html/body/div[1]/div/div/header/div[1]/div[1]/div/div[3]/div"
+    e_srh = None
+    e_srh = driver.find_element(By.XPATH, e_srh_path) #X if e_srh is not None:
+    time.sleep(1)
+    print("Type the stock name:", end=" ")
+    n = input()
+
+    #--- Clear the pop-up if it exists [Click] ---
+    clrPopup()
+    
+    #--- pyperclip ctrl+c +v & click the search box ---
+    pyperclip.copy(n) 
+    e_srh.click()
+    ActionChains(driver).key_down(Keys.CONTROL).send_keys("v").key_up(Keys.CONTROL).perform()
+
+    #--- js to click search ---
+    div_path = "/html/body/div[1]/div/div/header/div[1]/div[1]/div[1]/div[3]/div/div[4]" #X /svg
+    e_srhBtn = driver.find_element(By.XPATH, div_path)
+    time.sleep(1)
+    driver.execute_script("arguments[0].click();", e_srhBtn)
+    time.sleep(3)
+    # Doesn't work
+    # e_srh.clear() | e_srh.send_keys("Amata") | e_srh.send_keys(Keys.RETURN / Keys.ENTER) | e_srhBtn.click() | ActionChains(driver)       
+except:
+    print("Something wrong in the searching time")
+
+# At the searched page
+## Clear the pop-up if it exists [Click]
+clrPopup()
 
 # Close the browser
 driver.quit()
